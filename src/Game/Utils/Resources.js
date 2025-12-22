4b484a725b4be66d8373fa386689e583516fae8b
Hegyi Krisztián <hegyikrisztian1@gmail.com>
2025-12-22 08:37:19 +0100

    Add Resources class, init sources

---
+++ src/Game/Utils/Resources.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three'
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import EventEmitter from "./EventEmitter";
+
+
+export default class Resources extends EventEmitter {
+
+    constructor(sources) {
+        
+        super()
+
+        this.sources = sources
+
+        this.items = {}
+        this.toLoad = this.sources.length
+        this.loaded = 0
+
+        this.setLoaders()
+    }
+
+    setLoaders() {
+        this.loaders = {
+            gltfLoader: new GLTFLoader(),
+            cubeTextureLoader: new  THREE.CubeTextureLoader(),
+            textureLoader: new THREE.TextureLoader(),
+        }
+    }
+
+    startLoading() {
+        for (const source of this.sources) {
+            
+            this.loaders[source.loader].load(
+                source.path,
+                (file) => {
+                    this.sourceLoaded(source, file)
+                }
+            )
+        }
+    }
+
+    sourceLoaded(source, file) {
+        this.items[source.name] = file
+        this.loaded++
+
+        if (this.toLoad === this.loaded) {
+            this.trigger("ready")
+        }
+    }
+
+    setManager() {
+
+        this.manager = new THREE.LoadingManager(
+            () => console.log("Loaded everything"),
+            (url, loaded, total) => console.log(`Loading ${url}. Loaded ${loaded} of ${total} resources`),
+            (url) => console.warn(`Error loading ${url}`)
+        )
+
+        for (const loader of Object.values(this.loaders)) {
+            loader.manager = this.manager
+        }
+    }
+}
