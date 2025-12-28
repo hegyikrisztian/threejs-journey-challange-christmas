347ac3533628234e4bdc0e8a4f763256a773b5fa
Hegyi Krisztián <hegyikrisztian1@gmail.com>
2025-12-28 09:32:02 +0100

    Adjust sleigh models, add santa, add sleigh model in-game

--- src/Game/sources.js
+++ src/Game/sources.js
@@ -53,5 +53,20 @@ export default [
         path: "/models/present_type_2.glb",
         name: "present_2",
         loader: "gltfLoader"
-    }
+    },
+    {
+        path: "/models/chassy.glb",
+        name: "chassy",
+        loader: "gltfLoader"
+    },
+    {
+        path: "/models/runner_left.glb",
+        name: "runnerLeft",
+        loader: "gltfLoader"
+    },
+    {
+        path: "/models/runner_right.glb",
+        name: "runnerRight",
+        loader: "gltfLoader"
+    },
 ]
